5c8ba9f2ea4a90dc19ff2c5221a51d3a38dc2a22
atiqisrak <atiqisrak@gmail.com>
2023-05-13 23:58:48 +0600

    datasource updated to main domain

--- src/app/global/constants/datasource.js
+++ src/app/global/constants/datasource.js
@@ -1,13 +1,5 @@
 export const siteConstants = {
   // imageURL: "http://127.0.0.1:3000/images/",
   // imageURL: `${window.location.origin}/images/`,
-  imageURL: "/_next/images/",
+  imageURL: "https://sardarbikes.vercel.app/images/",
 };
-
-// import { useDynamicDomain } from "@/app/useDynamicDomain";
-
-// const domain = useDynamicDomain();
-
-// export const siteConstants = {
-//   imageURL: `${domain}/images/`,
-// };
