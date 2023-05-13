8c4d1b8418f164c415b21d2d965d10ae521f2871
atiqisrak <atiqisrak@gmail.com>
2023-05-13 13:28:55 +0600

    nn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <PromotionPoster />
       <ShipmentCTA />
       <Featured />
-      <Gallery />
+      {/* <Gallery /> */}
       {/* <Services /> */}
     </>
   );
