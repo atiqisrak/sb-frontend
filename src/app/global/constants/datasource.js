ee21a39f8472fa4d1ab1273ff2b4c2f4d5231cfc
atiqisrak <atiqisrak@gmail.com>
2023-05-13 14:37:50 +0600

    imgsrc updated

--- src/app/global/constants/datasource.js
+++ src/app/global/constants/datasource.js
@@ -1,7 +1,7 @@
 export const siteConstants = {
   // imageURL: "http://127.0.0.1:3000/images/",
   // imageURL: `${window.location.origin}/images/`,
-  imageURL: "/images/",
+  imageURL: "/_next/images/",
 };
 
 // import { useDynamicDomain } from "@/app/useDynamicDomain";
