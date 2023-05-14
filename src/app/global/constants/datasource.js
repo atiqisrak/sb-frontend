80c6f7d96b8e7921ba55783dd0423561dd1de2ff
atiqisrak <atiqisrak@gmail.com>
2023-05-14 20:40:32 +0600

    datasource window location

--- src/app/global/constants/datasource.js
+++ src/app/global/constants/datasource.js
@@ -1,5 +1,5 @@
 export const siteConstants = {
   // imageURL: "http://127.0.0.1:3000/images/",
-  // imageURL: `${window.location.origin}/images/`,
-  imageURL: "https://sardarbikes.vercel.app/images/",
+  imageURL: `${window.location.origin}/images/`,
+  // imageURL: "https://sardarbikes.vercel.app/images/",
 };
