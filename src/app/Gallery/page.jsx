253bcd5e7419c735572ff86a0e7a5a4dda5a11eb
atiqisrak <atiq.israk@webable.digital>
2023-06-14 18:39:56 +0600

    product gallery style done

--- src/app/Gallery/page.jsx
+++ src/app/Gallery/page.jsx
@@ -2,7 +2,9 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { siteConstants } from "../global/constants/datasource";
-import { Row, Table, Typography, Column, Card, Col } from "antd";
+import { Row, Table, Typography, Column, Card, Col, Tabs, Button } from "antd";
+import Icon from "@ant-design/icons/lib/components/Icon";
+import { RightOutlined } from "@ant-design/icons";
 const { Title, Paragraph } = Typography;
 const { Meta } = Card;
 
@@ -64,46 +66,217 @@ export default function Gallery() {
     return formattedNumber;
   };
 
+  const MAX_TITLE_LENGTH = 12;
+
   // Generate a row for each product group
   const rows = productGroups.map((group, index) => (
-    <Row key={`row${index}`} gutter={[16, 16]}>
+    <Row key={`row${index}`} gutter={[16, 8]}>
       {group.map((product) => (
         <Col key={product._id} span={8}>
-          <div>
-            <Title level={5}>
-              {product.description.brand} {product.description.model}
-            </Title>
-            <Image
-              src={"/Images/" + product.image}
-              width={200}
-              height={100}
-              alt="Product Picture"
-              // unoptimized="true"
-            />
-            <Paragraph>{product.description.mileage} KM</Paragraph>
-            <Paragraph>
-              ৳ {formatNumberWithCommas(product.offerPrice)}
-            </Paragraph>
+          <div
+            style={{
+              // width: "16em",
+              padding: "1rem 0.4rem",
+              margin: "1rem",
+              backgroundColor: "#F6F8F8",
+              borderRadius: "2.4rem",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                src={"/Images/" + product.image}
+                width={280}
+                height={190}
+                alt="Product Picture"
+                // unoptimized="true"
+              />
+            </div>
+
+            <div
+              className="prodDescription"
+              style={{
+                display: "flex",
+                alignItems: "space-between",
+                justifyContent: "center",
+                marginLeft: "2rem",
+                gap: "2rem",
+              }}
+            >
+              <div className="prodDesLeft">
+                <Title
+                  level={5}
+                  style={{
+                    fontSize: "1.2rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {/* {product.description.brand.length +
+                    product.description.model.length >
+                  MAX_TITLE_LENGTH
+                    ? `${product.description.brand} ${product.description.model}`.slice(
+                        0,
+                        MAX_TITLE_LENGTH
+                      ) + "..."
+                    : `${product.description.brand} ${product.description.model}`} */}
+                  {/* {product.description.brand} {product.description.model} */}
+                  {product.description.model}
+                </Title>
+                <Paragraph
+                  style={{
+                    fontSize: "0.9rem",
+                    fontWeight: 600,
+                    color: "#8E8E8E",
+                  }}
+                >
+                  {product.description.mileage} KM
+                </Paragraph>
+                <Paragraph
+                  style={{
+                    fontSize: "1.2rem",
+                    fontWeight: 600,
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#ff733b",
+                    }}
+                  >
+                    ৳
+                  </span>{" "}
+                  {formatNumberWithCommas(product.offerPrice)}
+                </Paragraph>
+              </div>
+              <div
+                className="prodDesRight"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                  }}
+                >
+                  <RightOutlined
+                    style={{
+                      fontSize: "1.5rem",
+                    }}
+                  />
+                </Button>
+              </div>
+            </div>
           </div>
         </Col>
       ))}
     </Row>
   ));
 
+  const onChange = (key) => {
+    console.log(key);
+  };
+  const items = [
+    {
+      key: "1",
+      label: `All`,
+      children: rows,
+    },
+    {
+      key: "2",
+      label: `125`,
+      children: rows,
+    },
+    {
+      key: "3",
+      label: `150`,
+      children: rows,
+    },
+    {
+      key: "4",
+      label: `155`,
+      children: rows,
+    },
+    {
+      key: "5",
+      label: `165`,
+      children: rows,
+    },
+  ];
+  const tabs = <Tabs defaultActiveKey="1" items={items} onChange={onChange} />;
+
   return (
     <>
-      <Title autoSize={true} level={2}>
-        Explore
-      </Title>
-      <Paragraph>The best & favourite bikes</Paragraph>
-      {/* <Image
-        src="/Images/promo.png"
-        alt="Promotional Campaign"
-        width={1440}
-        height={810}
-        className="promoCampDesk"
-      /> */}
-      <div>{rows}</div>
+      <div className="galleryContainer">
+        <div
+          className="titles"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: "5rem",
+          }}
+        >
+          <Title
+            autoSize={true}
+            level={1}
+            style={{
+              color: "#ff733b",
+              fontSize: "3rem",
+              fontWeight: 600,
+              lineHeight: "1.2rem",
+              zIndex: 1,
+            }}
+          >
+            Explore
+          </Title>
+          <Title
+            autoSize={true}
+            level={1}
+            style={{
+              color: "#7b7b7b20",
+              fontSize: "5rem",
+              fontWeight: 600,
+              lineHeight: "1.2rem",
+              position: "absolute",
+              marginTop: "-1.2rem",
+            }}
+          >
+            Explore
+          </Title>
+          <Paragraph
+            style={{
+              paddingTop: "1rem",
+              fontFamily: "Poppins",
+              color: "#7b7b7b",
+              fontSize: "1.2rem",
+              fontWeight: 400,
+              lineHeight: "1.2rem",
+              letterSpacing: "0.2rem",
+            }}
+          >
+            The best & favourite bikes
+          </Paragraph>
+        </div>
+        <div
+          className="productGallery"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {tabs}
+        </div>
+      </div>
     </>
   );
 }
